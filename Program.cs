606643b2e4ddca37e7e6d38ba872e12d595bbe94
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-13 14:59:01 +0530

    Add : Snake and Ladder game played with single player at start position 0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SnakeLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            SnakeLadder.SnakeladderGame();
+          
+        
+        }
+    }
+}
---
+++ SnakeLadder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class SnakeLadder
+    {
+        public static void SnakeladderGame()
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+           
+            
+            }
+        }
+    }
+
