d846d4ad2bfc8dcb1c6f5a402e80e8dbdd8b1bc3
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-14 09:23:42 +0530

    Add : Report the number of times the dice was played to win the game and also the position after every die role

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -12,52 +12,55 @@ namespace SnakeLadder
         {
             Console.WriteLine("Snake and Ladder game played with single player at start position 0");
             int position = 0;
-            int count = 0;
-            int lastPosition = 100;
+            int numberOfTimesDiceRolled = 0;
+
+            Console.WriteLine("Welcome Player 1, Let's start the Game");
+            Console.WriteLine("Player 1 is at position : " + position);
+
             Random random = new Random();
-            Random random1 = new Random();
-            for (int i = 0; i < lastPosition; i++)
+
+            while (position < 100)
             {
-                while (count <= lastPosition)
+                int temp = position;
+
+                int dice = random.Next(1, 7);
+                numberOfTimesDiceRolled++;
+
+                Console.WriteLine($"The Player rolls a dice and gets: {dice}");
+                Console.WriteLine($"Number of Times Dice rolled is: {numberOfTimesDiceRolled}");
+
+                int option = random.Next(0, 3);
+
+                if (option == 0)
                 {
-                    int result = random.Next(0, 3);
-                    int Dice = random1.Next(1, 7);
-                    if (position == 0)
-                    {
-                        if (result == 0)
-                        {
-                            Console.WriteLine("Snake");
-                            Console.WriteLine(" Player moves behind by the number of position received in the dice");
-                            position = position - Dice;
-                            Console.WriteLine("position moves back : " + position);
-                            Console.WriteLine("Dice Roll : " + Dice);
-                        }
-                        else if (result == 1)
-                        {
-                            Console.WriteLine("Ladder");
-                            Console.WriteLine(" Player moves ahead by the number of position received in the die");
-                            position = position + Dice;
-                            Console.WriteLine("position moves ahead : " + position);
-                            Console.WriteLine("Dice Roll : " + Dice);
-                        }
-                        else
-                        {
-                            Console.WriteLine("No Play");
-                            Console.WriteLine("Player Will Stay in the same position");
-                        }
-
-                    }
-                    else if (position == 100)
-                    {
-                        Console.WriteLine("Player has Won !!!");
-                        Console.WriteLine("Dice Roll : " + Dice);
-
-                    }
-                    count++;
+                    Console.WriteLine("Player stays at same Position");
+                }
+                else if (option == 1)
+                {
+                    Console.WriteLine("Player Moves Ahead by {0} from ladder", dice);
+                    position += dice;
+                }
+                else
+                {
+                    Console.WriteLine("Player Moves Behind by {0} from Snake", dice);
+                    position -= dice;
                 }
 
+                if (position < 0)
+                {
+                    position = 0;
+                }
+                else if (position > 100)
+                {
+                    position = temp;
+                }
+                Console.WriteLine("Player Current Position is at : {0}", position);
+
             }
+            Console.WriteLine("Player is at Position : {0}", position);
+            Console.WriteLine($"Total Number of Times Dice rolled is: {numberOfTimesDiceRolled}");
+
         }
-        }
+    }
     }
 
